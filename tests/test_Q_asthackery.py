5efe0cf53a5ecc33310649aa48709712d0444167
Jarrad Whitaker <akdor1154@gmail.com>
2020-10-23 12:28:59 +1100

    whitespace

--- tests/test_Q_asthackery.py
+++ tests/test_Q_asthackery.py
@@ -1,40 +1,40 @@
 from csql import Q, RenderedQuery
 
 def test_Q_str() -> None:
-    q = Q("select 1")
-    assert q.build() == RenderedQuery(
-        sql="select 1",
-        parameters=[]
-    )
+	q = Q("select 1")
+	assert q.build() == RenderedQuery(
+		sql="select 1",
+		parameters=[]
+	)
 
 def test_Q_lambda_str() -> None:
-    q = Q(lambda: "select 1")
-    assert q.build() == RenderedQuery(
-        sql="select 1",
-        parameters=[]
-    )
+	q = Q(lambda: "select 1")
+	assert q.build() == RenderedQuery(
+		sql="select 1",
+		parameters=[]
+	)
 
 def test_Q_lambda_fstr() -> None:
-    q = Q(lambda: f"select 1")
-    assert q.build() == RenderedQuery(
-        sql="select 1",
-        parameters=[]
-    )
+	q = Q(lambda: f"select 1")
+	assert q.build() == RenderedQuery(
+		sql="select 1",
+		parameters=[]
+	)
 
 def test_Q_lambda_fstr_no_assign() -> None:
-    # can't test the result of this, waahh, but the AST looks
-    # different and this used to crash.
-    Q(lambda: f"select 1")
-    pass
+	# can't test the result of this, waahh, but the AST looks
+	# different and this used to crash.
+	Q(lambda: f"select 1")
+	pass
 
 def test_Q_lambda_fstr_in_fn() -> None:
-    # test what happens when Q call is inside another function...
-    # yet another thing that broke my AST walker. I'm sure there
-    # will be many more.
-    ident = lambda x: x
-    q = ident(Q(lambda: f"select 1"))
+	# test what happens when Q call is inside another function...
+	# yet another thing that broke my AST walker. I'm sure there
+	# will be many more.
+	ident = lambda x: x
+	q = ident(Q(lambda: f"select 1"))
 
-    assert q.build() == RenderedQuery(
-        sql="select 1",
-        parameters=[]
-    )
+	assert q.build() == RenderedQuery(
+		sql="select 1",
+		parameters=[]
+	)
