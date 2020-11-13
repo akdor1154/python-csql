1e6e4741d012061db38af64eb5a9ecd4e3c10d00
Jarrad Whitaker <akdor1154@gmail.com>
2020-11-13 15:31:31 +1100

    deprecate lambda usage

--- csql/api.py
+++ csql/api.py
@@ -3,6 +3,9 @@ from types import FrameType
 import inspect
 from .input import asthackery, strparsing
 from .models.query import Query, Parameters, RenderedQuery
+from contextlib import contextmanager
+import warnings
+from textwrap import dedent
 
 def _getCallerFrame(qframe: Optional[FrameType]) -> FrameType:
 	assert qframe is not None, "CPython only! Go vote for PEP-501."
@@ -10,9 +13,25 @@ def _getCallerFrame(qframe: Optional[FrameType]) -> FrameType:
 	assert parentFrame is not None, "Couldn\'t get caller frame! Go whinge that PEP-501 never got implemented."
 	return parentFrame
 
+@contextmanager
+def noisyWarnings():
+	old_showwarning = warnings.showwarning
+	with warnings.catch_warnings():
+		warnings.simplefilter('always')
+		warnings.showwarning = old_showwarning
+		yield
 
 def Q(sql: Union[str, Callable[[], str]], parameters: Optional[Parameters] = None) -> Query:
 	if callable(sql):
+		with noisyWarnings():
+			warnings.warn(
+				dedent('''
+					Passing a lambda to Q is deprecated! You can now just pass an interpolated string directly:
+						Q(f"select from {blah}")
+				''').strip(),
+				category=DeprecationWarning,
+				stacklevel=2
+			)
 		callerFrame = _getCallerFrame(inspect.currentframe())
 		queryParts = asthackery.getQueryParts(sql, callerFrame)
 	else:
