f91ca0132d3f166af537fec42b58038c5f328803
Jarrad Whitaker <jwhitaker@officeworks.com.au>
2022-01-06 15:27:04 +1100

    add tags to caching, and switch back to asyncio method

--- csql/_/cacher/__init__.py
+++ csql/_/cacher/__init__.py
@@ -17,6 +17,9 @@ logger = logging.getLogger(name=__name__)
 class Persistable(QueryExtension):
     """ Attached directly to a slow query. """
     cacher: 'Cacher'
+    
+    tag: Optional[str]
+    'User-supplied tag, to potentially be used by Cacher to give readable names to things it caches.'
 
 def  _cache_replacer(queryRenderer: QueryRenderer) -> QueryReplacer:
     def replacer(q: Query) -> Query:
@@ -24,7 +27,7 @@ def  _cache_replacer(queryRenderer: QueryRenderer) -> QueryReplacer:
             return q
     
         # q is persistable.
-        save_fn = KL._make_save_fn(q, queryRenderer, p.cacher)
+        save_fn = KL._make_save_fn(q, queryRenderer, p.cacher, p.tag)
         return q._add_extensions(PreBuild(save_fn))
     return replacer
 
@@ -33,55 +36,55 @@ class KeyLookup():
     saved: Dict[int, Query] = {}
     lock = threading.Lock()
 
-    def _get_key(self, rq: RenderedQuery) -> int:
-        return hash((rq.sql, *rq.parameters))
+    def _get_key(self, rq: RenderedQuery, tag: Optional[str]) -> int:
+        return hash((rq.sql, tag, *rq.parameters))
 
-    def _make_save_fn(self, q: Query, qr: QueryRenderer, c: 'Cacher') -> Callable[[], Query]:
+    def _make_save_fn(self, q: Query, qr: QueryRenderer, c: 'Cacher', tag: Optional[str]) -> Callable[[], Query]:
         rq = qr.render(q)
-        key = self._get_key(rq)
+        key = self._get_key(rq, tag)
 
-        def wrapped_save_fn() -> Query:
-            with self.lock:
-                if key not in self.saved:
-                    logger.debug(f'Executing save function for rendered query {rq}')
-                    self.saved[key] = asyncio.run(c._persist(rq, key))
-                else:
-                    logger.debug(f'Using cached result for rendered query {rq}')
-                result = self.saved[key]
-            
-            return result
-            
-        return wrapped_save_fn
-        # async def wrapped_save_fn() -> Query:
-
+        # def wrapped_save_fn() -> Query:
         #     with self.lock:
         #         if key not in self.saved:
-        #             loop = asyncio.get_event_loop()
-        #             # we have to make our own future here so we can await it multiple times if necessary.
-        #             # if we were to put the task from _persist() into saved, we could only await it once.
-        #             future = loop.create_future()
-        #             self.saved[key] = future
-        #             asyncio \
-        #                 .create_task(c._persist(rq, key)) \
-        #                 .add_done_callback(lambda t: future.set_result(t.result()))
-        #         result_future = self.saved[key]
-                
-        #     result = await result_future
-        #     print(f'{result=}')
+        #             logger.debug(f'Executing save function for rendered query {rq} with {tag=}')
+        #             self.saved[key] = asyncio.run(c._persist(rq, key, tag))
+        #         else:
+        #             logger.debug(f'Using cached result for rendered query {rq} with {tag=}')
+        #         result = self.saved[key]
+            
         #     return result
+            
+        # return wrapped_save_fn
+        
+        async def wrapped_save_fn() -> Query:
+            with self.lock:
+                if key not in self.saved:
+                    loop = asyncio.get_event_loop()
+                    # we have to make our own future here so we can await it multiple times if necessary.
+                    # if we were to put the task from _persist() into saved, we could only await it once.
+                    future = loop.create_future()
+                    self.saved[key] = future
+                    asyncio \
+                        .create_task(c._persist(rq, key, tag)) \
+                        .add_done_callback(lambda t: future.set_result(t.result()))
+                result_future = self.saved[key]
+                
+            result = await result_future
+            print(f'{result=}')
+            return result
 
-        # return lambda: asyncio.run(wrapped_save_fn())
+        return lambda: asyncio.run(wrapped_save_fn())
 
 KL = KeyLookup() # singleton
 
 class Cacher(ABC):
 
-    def persist(self, q: Query) -> Query:
+    def persist(self, q: Query, tag: Optional[str]) -> Query:
         """ Marks a query as persistabe. """
-        return q._add_extensions(Persistable(self))
+        return q._add_extensions(Persistable(self, tag))
 
     @abstractmethod
-    async def _persist(self, rq: RenderedQuery, key: int) -> Query:
+    async def _persist(self, rq: RenderedQuery, key: int, tag: Optional[str]) -> Query:
         """ Returns a function which will save the query, and a function which returns a query that will retrieve the saved one. """
         pass
 
@@ -90,8 +93,8 @@ class TempTableCacher(Cacher):
     def __init__(self, connection: Any):
         self._con = connection
 
-    async def _persist(self, rq: RenderedQuery, key: int) -> Query:
-        table_name = f'"csql_cache_{uuid.uuid4()}"'
+    async def _persist(self, rq: RenderedQuery, key: int, tag: Optional[str]) -> Query:
+        table_name = f'"csql_cache_{tag}_{key}"'
 
         sql, params = rq
 
