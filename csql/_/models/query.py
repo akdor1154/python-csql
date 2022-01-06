35a75b460fc9469aff16c4a47d93b48e11d87fb1
Jarrad Whitaker <jwhitaker@officeworks.com.au>
2022-01-06 11:25:19 +1100

    parameters code cleanup

--- csql/_/models/query.py
+++ csql/_/models/query.py
@@ -26,73 +26,13 @@ else:
 
 ScalarParameterValue = Any
 
-class ParameterList(List[ScalarParameterValue]):
-	"""This is designed to be returned and passed directly to your DB API. It acts like a list."""
-
-	_params: List[ScalarParameterValue]
-	_paramKeys: Dict[str, List[List[int]]]
-
-	def __getitem__(self, i: Any) -> ScalarParameterValue:
-		return self._params[i]
-
-	def __iter__(self) -> Iterator[ScalarParameterValue]:
-		return iter(self._params)
-
-	def __len__(self) -> int:
-		return len(self._params)
-
-	def __init__(self, *params: ScalarParameterValue, keys: Dict[str, List[List[int]]] = {}) -> None:
-		self._params = list(params)
-		self._paramKeys = keys
-
-	def __repr__(self) -> str:
-		return f'Params {repr(self._params)}'
-
-	def __eq__(self, other: Any) -> bool:
-		if isinstance(other, ParameterList):
-			return self._params == other._params
-		elif isinstance(other, list): # mainly used so I didn't have to rewrite a bunch of tests.. this may be removed.
-			return self._params == other
-		else:
-			return False
-
-	def _reparameterize(self, replacements: Dict[str, Union[Collection[ScalarParameterValue], ScalarParameterValue]]) -> List[ScalarParameterValue]:
-
-		newParams = self._params.copy()
-
-		for key, paramValue in replacements.items():
-			if key not in self._paramKeys:
-				raise Exception(f'Replacement param {key} is not in the original query. You might mean one of {",".join(self._paramKeys)}')
-
-			instances = self._paramKeys[key]
-
-			for indices in instances:
-				paramValue = replacements[key]
-				if not isinstance(paramValue, CollectionABC) or isinstance(paramValue, str):
-					paramValue = [paramValue]
-
-				if len(paramValue) != len(indices):
-					origValues = [
-						self._params[i] for i in indices
-					]
-					raise ValueError(
-						'Replacing collections is only OK if the replacement is the same length as the original.\n'
-						f'You attempted to replace {repr(key)} = {repr(origValues)} (length {len(indices)}) with {repr(paramValue)} (length {len(paramValue)})'
-					)
-				for i, replacement in zip(indices, paramValue):
-					newParams[i] = replacement
-
-		return newParams
-
-	def reparameterize(self, **replacements: ScalarParameterValue) -> 'ParameterList':
-		return ParameterList(
-			*self._reparameterize(replacements),
-			keys=self._paramKeys
-		)
+TParameterList = Sequence[ScalarParameterValue]
+def ParameterList(*s: ScalarParameterValue) -> TParameterList:
+	return list(s)
 
 class RenderedQuery(NamedTuple):
 	sql: str
-	parameters: ParameterList
+	parameters: TParameterList
 
 	# utility properties for easy splatting
 	@property
@@ -103,7 +43,7 @@ class RenderedQuery(NamedTuple):
 		)
 
 	@property
-	def db(self) -> Tuple[str, ParameterList]:
+	def db(self) -> Tuple[str, TParameterList]:
 		return (self.sql, self.parameters)
 
 class QueryBit(metaclass=ABCMeta):
@@ -282,7 +222,7 @@ def _params_replacer(newParams: Optional[Dict[str, Any]]) -> QueryReplacer:
 		assert newParams is not None # make mypy happy
 
 		if (isinstance(p, ParameterPlaceholder) and p.key in newParams):
-			return ParameterPlaceholder(key=p.key, value=newParams[p.key], parameters=None)
+			return ParameterPlaceholder(key=p.key, value=newParams[p.key], _key_context=None)
 		else:
 			return p
 
@@ -308,7 +248,7 @@ def _pre_build_replacer() -> QueryReplacer:
 class ParameterPlaceholder(QueryBit, InstanceTracking):
 	key: str
 	value: Any
-	parameters: 'Parameters'
+	_key_context: Optional[int] # allow people to pass multiple distinct parameters with the same key into a Query.
 
 
 class Parameters:
@@ -355,8 +295,8 @@ class Parameters:
 
 	def __getitem__(self, key: str) -> ParameterPlaceholder:
 		paramVal = self.params[key] # check existence
-		return ParameterPlaceholder(key=key, value=paramVal, parameters=self)
+		return ParameterPlaceholder(key=key, value=paramVal, _key_context=id(self))
 
 	def __getattr__(self, key: str) -> ParameterPlaceholder:
 		paramVal = self.params[key] # check existence
-		return ParameterPlaceholder(key=key, value=paramVal, parameters=self)
+		return ParameterPlaceholder(key=key, value=paramVal, _key_context=id(self))
